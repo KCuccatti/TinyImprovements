6ebcb49c127d4b757b3fec44e76a5d7a83748f99
Kyle Cuccatti <kylecuccatti@ymail.com>
2018-11-14 19:02:49 -0600

    Fixing remote host

--- models/index.js
+++ models/index.js
@@ -1,11 +1,4 @@
 "use strict";
-var dotenv = require('dotenv').config();
-var fs = require("fs");
-var path = require("path");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-console.log("The Environment is: " + env);
-var config = require(__dirname + '/../config/config.js')[env];
 
 const mongoose = require('mongoose');
 const User = require('./User.js');
