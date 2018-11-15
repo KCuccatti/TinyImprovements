b6c1141ef73a3ce8ab5e417b1f010209321e70e2
Kyle Cuccatti <kylecuccatti@ymail.com>
2018-11-14 19:14:36 -0600

    Refactored server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+
+/*const express = require('express');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,29 @@ app.use(express.static("public"));
 
 //mongoose.connect('mongodb://localhost/kudos_db', { useNewUrlParser: true });
 
+require('./routes/api-routes')(app);
+
+
+app.listen(PORT, function() {
+  console.log(`App running on port ${PORT}`);
+});
+*/
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+//mongoose.connect('mongodb://localhost/kudos_db', { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kudos_db";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 require('./routes/api-routes')(app);
 
 
