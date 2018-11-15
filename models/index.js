90b25f8ba1f2765c222a335d903fa97fb37dfd14
Kyle Cuccatti <kylecuccatti@ymail.com>
2018-11-14 18:55:12 -0600

    trying again

--- models/index.js
+++ models/index.js
@@ -11,31 +11,12 @@ const mongoose = require('mongoose');
 const User = require('./User.js');
 const Kudos = require('./Kudos.js')
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kudos_db";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 module.exports = {
     User: User,
     Kudos: Kudos
-}
-
-if (env==="production") {
-  console.log("Using PROD STUFF");
-  console.log("Using Environment Variables");
-  console.log("User Name: " + process.env.MONGO_PROD_USER);
-  console.log("Database: " + process.env.MONGO_PROD_DBNAME);
-  console.log("Host: " + process.env.MONGO_PROD_HOST);
-  console.log("THE DATABSE: " + config.database);
-
-  var dbUrl = 'mongodb://' + process.env.MONGO_PROD_USER +":" + process.env.MONGO_PROD_KEY + "@" + 
-  process.env.MONGO_PROD_HOST + "/" + process.env.MONGO_PROD_DBNAME + "?authSource=yourDB&w=1";
-  console.log(dbUrl);
-
-  mongoose.connect(dbUrl, { useNewUrlParser: true });
-  
-} else {
-  console.log("Using DEV stuff");
-  console.log("Using Environment Variables");
-  console.log("Database: " + process.env.MYSQL_DEV_DBNAME);
-  console.log("Host: " + process.env.MYSQL_DEV_HOST);
-  console.log("Getting ready to connect to db...");
-  mongoose.connect('mongodb://localhost/kudos_db', { useNewUrlParser: true });
 }
 
